62d602284d00edc952988d3d6ed70c99587f5324
VanyaMayazura <90196679+vnymyz@users.noreply.github.com>
2023-11-10 13:32:20 +0700

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,6 +3,6 @@ import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "development"
     ? undefined
-    : "https://api.render.com/deploy/srv-cl1itn8310os73da4eag?key=y5jFOc1d_yM";
+    : "localhost:4000";
 
 export const socket = io(URL);
